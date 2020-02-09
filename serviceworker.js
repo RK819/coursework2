b963ba6f4628454b5b59127febb2174cd71145e3
kamal <kamal1231789@gmail.com>
2020-02-09 23:26:49 +0000

    update

--- serviceworker.js
+++ serviceworker.js
@@ -16,50 +16,50 @@ var appShellFiles = [
 
 var contentToCache = [] = appShellFiles;
 
-self.addEventListener('install', (e) => {
+self.addEventListener('install',(e)=>{
     console.log('[Service Worker] Install');
     e.waitUntil(
-   caches.open(cacheName).then((cache) => { 
-   console.log('[Service Worker] Caching all: app shell and content');
-   return cache.addAll(contentToCache);
-   })
-   );
-   });
+        caches.open(cacheName).then((cache) => {
+             console.log('[Service Worker] Caching all: app shell and content');
+             return cache.addAll(contentToCache);
+            })
+            );
+        }
+ );
 
-   self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function(r){
-            console.log('[Service Worker] Fetching resource: '+e.request.url);
-            return r || fetch(e.request).then( function (response) {
-                return caches.open(cacheName).then(function (cache)
-                {
-               console.log('[Service Worker] Caching new resource: '+e.request.url);
-               cache.put(e.request, response.clone());
-               return response;
-           });
-       });
-   }));
+ self.addEventListener('fetch', function (e) {
+     e.respondWith(
+         caches.match(e.request).then(function(r){
+             console.log('[Service Worker] Fetching resource: '+e.request.url);
+             return r || fetch(e.request).then( function (response) {
+                 return caches.open(cacheName).then(function (cache)
+                 {
+                console.log('[Service Worker] Caching new resource: '+e.request.url);
+                cache.put(e.request, response.clone());
+                return response;
+            });
+        });
+    }));
 });
 
 self.addEventListener('install',(e) => {
-   e.waitUntil(
-   caches.open('cacheFiles-v1').then((cache) => {
-       return cache.addAll(contentToCache);
-   }));
+    e.waitUntil(
+    caches.open('cacheFiles-v1').then((cache) => {
+        return cache.addAll(contentToCache);
+    }));
 });
 
 self.addEventListener('activate',(e) => {
 
-   e.waitUntil(
-   caches.keys().then((keyList) => {
-   return Promise.all(keyList.map((key) => { 
-   if (key !== cacheName) { 
-   return caches.delete(key);
-   }}
-   ));
-   }));
-   });
-
+    e.waitUntil(
+    caches.keys().then((keyList) => {
+    return Promise.all(keyList.map((key) => { 
+    if (key !== cacheName) { 
+    return caches.delete(key);
+    }}
+    ));
+    }));
+    });
 
 
   
